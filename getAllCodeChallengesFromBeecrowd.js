a54379f3286b17a052e608af43d21451c853fe9a
kaedh <leonardomds2013@gmail.com>
2021-12-04 18:55:47 -0300

    finish script to scrap data from uri

--- getAllCodeChallengesFromBeecrowd.js
+++ getAllCodeChallengesFromBeecrowd.js
@@ -1,30 +1,60 @@
 const puppeteer = require('puppeteer');
+const fs = require("fs");
 
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://www.beecrowd.com.br/judge/pt/problems/all');
+const getAllExercicesPerPage = () => {
+  const trNode = [...document.querySelectorAll("tr")];
   
-  
-  const node = await page.evaluate(() => {
-    const trNode = [...document.querySelectorAll("tr")];
+  const data = trNode.map(node => {
+    const challengeId = node.children[0]?.textContent;
+    const challengeUrl = node.children[2]?.children[0]?.href;
 
-    const data = trNode.map( (node) => {
-      const challengeId = node.children[0].textContent;
-      const challengeUrl = node.children[2].children[0].href
-
-      return {
-        codeId: challengeId,
-        codeUrl: challengeUrl
-      }
-    } )
-
-
-    return data;
+    return { codeId: challengeId, codeUrl: challengeUrl }
   })
 
-  console.log(node)
+  data.shift()
 
+  return data;  
+  
+}
+
+const sanitizeDataScrapped = (data) => {
+
+  const sanitizedData = data.map( exercise => {
+    if (exercise.codeId != "") {
+      exercise.codeId = exercise.codeId.slice(1).trim()
+
+      return exercise
+    }
+  })
+
+  return sanitizedData
+} 
+
+const main = async () => {
+  const browser = await puppeteer.launch();
+
+  let data = [];
+  let dataPerPage = [];
+
+  for(let i = 1; i <= 92; i++) {
+    const pageUrl = `https://www.beecrowd.com.br/judge/pt/problems/all?page=${i}`
+
+    const page = await browser.newPage();
+    await page.goto(pageUrl);
+
+    dataPerPage = await page.evaluate(getAllExercicesPerPage)
+
+    data = [...data, ...dataPerPage ];
+    
+  }
+
+  const sanitizedData = sanitizeDataScrapped(data);
+
+  fs.writeFile("data.json", JSON.stringify({...sanitizedData}), (err) => {
+    if (err) console.log(err)
+  })
 
   await browser.close();
-})();
+}
+
+main();
