697f30199d4a82a6234cfce0b0f2c723b72ea147
kaedh <leonardomds2013@gmail.com>
2021-12-06 00:33:00 -0300

    finish code challenge getter

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
+const { getChallenge } = require("../services/code.solving.services");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
+  getChallenge()
   res.render('index', { title: 'Express' });
 });
 
--- src/services/code.solving.services.js
+++ src/services/code.solving.services.js
@@ -1,5 +1,84 @@
-/*
-  marcar como resolvido
-  receber um desafio nao resolvido aleatorio
+const db = require("../data/db");
 
-*/
+function randomNumber(min, max) { 
+  return parseInt(Math.abs(Math.random() * (max - min) + min));
+} 
+
+const _markAsActive = async (id) => {
+  try {
+    await db('tbcode').where({id: id}).update({activechallenge: true})
+  
+  } catch (err) {
+    console.log({ error: true, message: err })
+  }
+}
+
+const _unmarkAsActive = async (id) => {
+  try {
+    await db('tbcode').where({id: id}).update({activechallenge: false})
+    
+  } catch (err) {
+    console.log({ error: true, message: err })
+  }
+}
+
+const _checkIfHasChallengeActive = async () => {
+  try {
+    const [ result ] = await db('tbcode').where({activechallenge: true})
+
+    return result
+    
+  } catch (err) {
+    console.log({ error: true, message: err })
+  }
+}
+
+const markAsSolved = async (id) => {
+  try {
+
+    await db('tbcode').where({id: id}).update({solved: true})
+    await _unmarkAsActive(id)
+    
+  } catch (err) {
+    console.log({ error: true, message: err })
+  }
+}
+
+
+const getChallenge = async () => {
+  try {
+    const challengeActive = await _checkIfHasChallengeActive()
+    if(challengeActive) return challengeActive
+
+
+    let validCode = false;
+    const [maxIdResult] = await db('tbcode').count('*')
+
+    const maxInterval = maxIdResult.count
+    const minInterval = 1
+
+    while(!validCode) {
+      const randomCode = randomNumber(minInterval, maxInterval)
+      const [result] = await db('tbcode').where({id: randomCode})
+
+      if (result.solved == false) {
+        _markAsActive(result.id)
+        validCode = true
+        return result
+      }  
+    }
+
+     
+
+  } catch (err) {
+    console.log({ error: true, message: err })
+  }
+}
+
+
+
+
+module.exports = {
+  markAsSolved,
+  getChallenge
+};
